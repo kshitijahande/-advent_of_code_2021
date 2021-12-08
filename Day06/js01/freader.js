a65892c00b003c9d1ad959fc6d257f1bfea96658
nikshinde1996 <crenickshinde1996@gmail.com>
2021-12-08 22:53:20 +0530

    Add solution for AOC day-06 in javascript

---
+++ Day06/js01/freader.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+const data = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+    .split(',')
+    .map((num) => parseInt(num, 10));
+
+module.exports = {
+	data,
+};
